22bd4a1316aa1429da7fb9b849c8a14ca99e41bb
bruce <35551306+deftthunk@users.noreply.github.com>
2018-09-18 23:57:49 -0400

    fixed output

--- strMob.sh
+++ strMob.sh
@@ -2,13 +2,6 @@
 
 threads=4
 
-process () {
-	f=$1
-	n=$2
-	grep -P "[0-9A-Za-z]{$n}" .tmpFile4 >> .out.$f
-	rm .tmpFile*
-}
-
 parse () {
 	f=$1
 	fname=`basename $1`
@@ -18,41 +11,43 @@ parse () {
 	strings -a -el -n5 --radix=d $f > .strings_el_$fname & \
 	strings -a -eL -n5 --radix=d $f > .strings_eL_$fname & \
 	strings -a -eb -n5 --radix=d $f > .strings_eb_$fname & \
-	strings -a -eB -n5 --radix=d $f > .strings_eB_$fname & \
-	strings -a -es -n5 --radix=d $f > .strings_es_$fname & 
+	strings -a -eB -n5 --radix=d $f > .strings_eB_$fname &
 
 	while [[ `ps -C strings --no-header` ]]; do
 		sleep 1
 	done
 }
 
+process () {
+	for num in {5..10}; do
+		grep -P --binary-files=text "^\s*?\d+\s.*?[0-9A-Za-z]{$num}" .strall >> .out.$name
+	done
+
+	# replace non-ascii chars with whitespace
+	perl -i -pe 's/[^[:ascii:]]/ /g' .out.$name
+	sed -i 's/\s\s+/\s/g' .out.$name
+}
+
 sorting () {
-	sort -k2 --ignore-nonprinting --parallel=$threads -o .tmpFile1 .strings_$1
+	sort -k2 --ignore-nonprinting --parallel=$threads -o .tmpFile1 .out.$name
 	uniq -f1 -i .tmpFile1 .tmpFile2
 	sort -k1 -n --parallel=$threads -o .tmpFile3 .tmpFile2
-	awk '{print $2}' .tmpFile3 > .tmpFile4
+
+	# print all but first column, removing file indexing but preserving order
+	awk '{$1=""; print $0}' .tmpFile3 > final
 }
 
 for file in $@; do
 	path=$file
 	name=`basename $file`
 	parse $path $name
-	cat .strings_* > .strings_$name
+	cat .strings_* > .strall
 
-	sorting $name
+	process
+	sorting
+
+	# clean up
+	rm .tmpFile* .strings_* .out.$name .strall
 	
-	# target different human-readable string lengths
-	for num in {5..10}; do
-		process $name $num
-	done
-	# clean up previous run
-	rm .strings_*
-	mv .out.$name .strings_$name
-	
-	# last pass
-	sorting $name 
-	process $name 1
-	mv .out.$name strings.$name
-	rm .out*
 done
 
