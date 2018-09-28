d9e6af165a5a9016ff5842e6c9a58c79e759bf34
bruce <35551306+deftthunk@users.noreply.github.com>
2018-09-27 21:13:51 -0400

    added indexing and progress ruler; improved sorting speed; added status to STDOUT during runtime

--- strMob.sh
+++ strMob.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 threads=4
+minLen=5
 
 parse () {
 	f=$1
 	fname=`basename $1`
+	echo "> Finding 7/16/32-bit (big/little)endian"
 
-	strings -a -es -n5 --radix=d $f > .strings_es_$fname & \
-	strings -a -eS -n5 --radix=d $f > .strings_eS_$fname & \
-	strings -a -el -n5 --radix=d $f > .strings_el_$fname & \
-	strings -a -eL -n5 --radix=d $f > .strings_eL_$fname & \
-	strings -a -eb -n5 --radix=d $f > .strings_eb_$fname & \
-	strings -a -eB -n5 --radix=d $f > .strings_eB_$fname &
+	strings -a -es -n $minLen --radix=d $f > .strings_es_$fname & \
+	strings -a -eS -n $minLen --radix=d $f > .strings_eS_$fname & \
+	strings -a -el -n $minLen --radix=d $f > .strings_el_$fname & \
+	strings -a -eL -n $minLen --radix=d $f > .strings_eL_$fname & \
+	strings -a -eb -n $minLen --radix=d $f > .strings_eb_$fname & \
+	strings -a -eB -n $minLen --radix=d $f > .strings_eB_$fname &
 
 	while [[ `ps -C strings --no-header` ]]; do
 		sleep 1
@@ -19,22 +21,41 @@ parse () {
 }
 
 process () {
-	for num in {5..10}; do
-		grep -P --binary-files=text "^\s*?\d+\s.*?[0-9A-Za-z]{$num}" .strall >> .out.$name
-	done
+	echo "> Prune unlikely candidates"
+	grep -P --binary-files=text "^\s*?\d+\s.*?[0-9A-Za-z]{$minLen}" .strall > .out.$name
 
-	# replace non-ascii chars with whitespace
-	perl -i -pe 's/[^[:ascii:]]/ /g' .out.$name
+	echo "> Replace non-ascii chars with whitespace"
+	perl -i -pe 's/[^[:ascii:]]/\s/g' .out.$name
+
+	echo ">    --Pass 1/3"
 	sed -i 's/\s\s+/\s/g' .out.$name
+
+	echo ">    --Pass 2/3"
+	sed -i 's/^\s//g' .out.$name
+
+	echo ">    --Pass 3/3"
+	sed -i 's/\s$//g' .out.$name
 }
 
 sorting () {
+	echo "> Sort and uniq"
 	sort -k2 --ignore-nonprinting --parallel=$threads -o .tmpFile1 .out.$name
 	uniq -f1 -i .tmpFile1 .tmpFile2
+
+	echo "> Rearrange strings to original ordering"
 	sort -k1 -n --parallel=$threads -o .tmpFile3 .tmpFile2
 
 	# print all but first column, removing file indexing but preserving order
-	awk '{$1=""; print $0}' .tmpFile3 > final
+	awk '{$1=""; print $0}' .tmpFile3 > .final
+
+	echo "> Adding index and progress ruler"
+	`cat .final | perl -ne '$l=0; open(FH, ".final");' \
+		-e '$l++ while(<FH>); close(FH);' \
+		-e '$chunk = ($l / 100);' \
+		-e '$ln=0; $p=1; foreach(<>) {' \
+		-e 'print("$ln-$p%:\t$_"); $ln++;' \
+		-e 'if($ln > ($chunk * $p)) {$p++;}' \
+		-e '}' > strings.$name`
 }
 
 for file in $@; do
@@ -46,8 +67,7 @@ for file in $@; do
 	process
 	sorting
 
-	# clean up
-	rm .tmpFile* .strings_* .out.$name .strall
+	echo "> Cleanup"
+	rm .tmpFile* .strings_* .out.$name .strall .final
 	
 done
-
